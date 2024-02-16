5d618cf5c30fe14099e5d19df5dcb2a5eade72ce
Eduardo Guarda <eduardo.guarda.97@gmail.com>
2024-02-16 14:34:57 -0300

    se agregan funciones q1_time y q1_memory

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,35 @@
 from typing import List, Tuple
 from datetime import datetime
+import json
+import time
 
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    with open(file_path, 'r') as f:
+        dates_dict = {}
+        
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            
+            tweet = json.loads(line)
+            date = tweet['date'].split('T')[0]
+            username = tweet['user']['username']
+            if date not in dates_dict.keys():
+                dates_dict[date] = {username: 1}
+            else:
+                if username not in dates_dict[date].keys():
+                    dates_dict[date][username] = 1
+                else:
+                    dates_dict[date][username] += 1
+    
+    top_dates = sorted(dates_dict.keys(), key=lambda x: sum(dates_dict[x].values()), reverse=True)
+    top_dates = top_dates[:10] if len(top_dates) > 10 else top_dates
+    top_users = [max(dates_dict[i].keys(), key=lambda x: dates_dict[i][x]) for i in top_dates]
+    return [(datetime.strptime(top_dates[i], "%Y-%m-%d").date(), top_users[i]) for i in range(len(top_dates))]
+
+if __name__ == '__main__':
+    initial_time = time.time()
+    return_list = q1_memory(file_path="farmers-protest-tweets-2021-2-4.json")
+    print(return_list)
+    print(f'EXECUTION TIME: {time.time() - initial_time}')
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,23 @@
 from typing import List, Tuple
 from datetime import datetime
+import pandas as pd
+import time
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    df = pd.read_json(file_path, lines=True).loc[:, ['date', 'user']]
+    df.date = pd.to_datetime(df.date).dt.date
+    top_dates = df['date'].value_counts(ascending=False).index.to_list()
+    top_dates = top_dates[:10] if len(top_dates) > 10 else top_dates
+    df['username'] = df['user'].apply(lambda x: x['username'])
+    df = df.loc[df['date'].isin(top_dates)] 
+    top_users = [df.loc[df['date']==date, 'username'].value_counts(ascending=False).index.to_list()[0] for date in top_dates]
+    return [(top_dates[i], top_users[i]) for i in range(len(top_dates))]
+
+
+if __name__ == '__main__':
+    initial_time = time.time()
+    return_list = q1_time(file_path="farmers-protest-tweets-2021-2-4.json")
+    
+    print(return_list)
+    print(f'EXECUTION TIME: {time.time() - initial_time}')
+
