41b358f1db701e7225c0ed42c9e97225ffe0ae95
Eduardo Guarda <eduardo.guarda.97@gmail.com>
2024-02-16 18:26:42 -0300

    se agrega codigos de q2_time y q2_memory

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,32 @@
 from typing import List, Tuple
+from datetime import datetime
+import json
+from emoji import emoji_list
+import time
 
-def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+def q2_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
+    with open(file_path, 'r') as f:
+        emoji_counter = {}
+        
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            
+            tweet_content = json.loads(line)['content']
+            tweet_content = emoji_list(tweet_content)
+            for emoji_obj in tweet_content:
+                emoji = emoji_obj['emoji']
+                if emoji not in emoji_counter.keys():
+                    emoji_counter[emoji] = 1
+                else:
+                    emoji_counter[emoji] += 1
+    
+    top_emojis = sorted(emoji_counter.keys(), key=lambda x: emoji_counter[x], reverse=True)[:10]
+    return [(emoji, emoji_counter[emoji]) for emoji in top_emojis]
+
+if __name__ == '__main__':
+    initial_time = time.time()
+    return_list = q2_memory(file_path="farmers-protest-tweets-2021-2-4.json")
+    print(return_list)
+    print(f'EXECUTION TIME: {time.time() - initial_time}')
--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,27 @@
 from typing import List, Tuple
+import json
+from emoji import emoji_list
+import pandas as pd
+import time
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    with open(file_path, 'r') as f:
+        tweets = f.readlines()
+
+    single_string = ""
+    for tweet in tweets:
+        single_string += json.loads(tweet)['content']
+    
+    emojis = emoji_list(single_string)
+    df = pd.DataFrame.from_dict(emojis)
+    emoji_counter = df['emoji'].value_counts(sort=True, ascending=False)
+    emoji_counter = emoji_counter.iloc[:10].to_dict()
+    keys = emoji_counter.keys()
+    
+    return [(key, emoji_counter[key]) for key in keys]
+
+if __name__ == "__main__":
+    initial_time = time.time()
+    return_value = q2_time(file_path="farmers-protest-tweets-2021-2-4.json")
+    print(return_value)
+    print(f"EXECUTION TIME: {time.time() - initial_time}")
