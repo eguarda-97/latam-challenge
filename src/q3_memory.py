2e71b3e5075fec05229a6898db5fc4b5213cc887
Eduardo Guarda <eduardo.guarda.97@gmail.com>
2024-02-18 05:58:42 -0300

    se agregan funciones de q3

--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,4 +1,28 @@
 from typing import List, Tuple
+import time
+import json
+from collections import Counter
+
 
 def q3_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    users_counter = Counter()
+    with open(file_path, 'r') as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            
+            mentioned_users = json.loads(line)['mentionedUsers']
+            if mentioned_users is None:
+                continue
+            
+            usernames = [user['username'] for user in mentioned_users]
+            users_counter.update(usernames)
+
+    return users_counter.most_common(10)
+
+if __name__ == '__main__':
+    initial_time = time.time()
+    return_list = q3_memory(file_path="farmers-protest-tweets-2021-2-4.json")
+    print(return_list)
+    print(f'EXECUTION TIME: {time.time() - initial_time}')
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,17 @@
 from typing import List, Tuple
+from collections import Counter
+import json
+
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    with open(file_path, 'r') as f:
+        data = [json.loads(line)['mentionedUsers'] for line in f.readlines()]
+    
+    usernames = [user['username'] for obj in data if obj is not None for user in obj]
+    return Counter(usernames).most_common(10)
+
+if __name__ == "__main__":
+    return_list = q3_time(file_path="farmers-protest-tweets-2021-2-4.json")
+    print(return_list)
+
+
